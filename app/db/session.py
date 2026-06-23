2554f2a4995ef3dc157ece749d764268d22b4415
Deepakrana123 <rdev69146@gmail.com>
2026-06-23 08:50:12 +0530

    database-issue

--- app/db/session.py
+++ app/db/session.py
@@ -5,22 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 print(os.getenv("DATABASE_URL"))
-DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set")
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,
-    pool_recycle=300,
-    pool_size=10,
-    max_overflow=20,
-    echo=False,
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_db():
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if not DATABASE_URL:
+        raise ValueError("DATABASE_URL is not set")
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True,
+        pool_recycle=300,
+        pool_size=10,
+        max_overflow=20,
+        echo=False,
+    )
+
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
     db = SessionLocal()
     try:
         yield db
